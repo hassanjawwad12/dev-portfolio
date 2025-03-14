05df253930384af42d47ceb1a482995738bec733
hassanjawwad12 <hassanjawwad01@gmail.com>
2025-03-14 14:46:34 +0500

    🎨 Added Links to Projects

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { FaLocationArrow } from "react-icons/fa6";
-
 import { projects } from "@/data";
 import { PinContainer } from "./ui/Pin";
 
@@ -19,8 +18,8 @@ const RecentProjects = () => {
             key={item.id}
           >
             <PinContainer
-              title={item.link}
-              href="https://twitter.com/mannupaaji"
+              title={item.title}
+              href={item.link}
             >
               <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
                 <div
@@ -65,12 +64,13 @@ const RecentProjects = () => {
                   ))}
                 </div>
 
-                <div className="flex justify-center items-center">
-                  <p className="flex lg:text-xl md:text-xs text-sm text-purple">
+                <a href={item.link} target="_blank" rel="noopener noreferrer" className="flex justify-center items-center">
+                <p className="flex lg:text-xl md:text-xs text-sm text-purple">
                     Check Live Site
                   </p>
                   <FaLocationArrow className="ms-3" color="#CBACF9" />
-                </div>
+                </a>
+
               </div>
             </PinContainer>
           </div>
